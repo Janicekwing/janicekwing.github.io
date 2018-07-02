54129afeac63667ff722ecb0c5717ac915b33e12
Janice He <janicekwhe@gmail.com>
2018-07-02 17:27:20 -0400

    Introducing PHP to make contact form accessible

---
+++ contact-form-handler.php
@@ -0,0 +1,24 @@
+<? php
+    $name =$_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = "guest@gmail.com";
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "Name: $name.\n".
+                    "Email: $visitor_email.\n".
+                        "Message: $message.\n";
+
+    $to = "janicekwhe@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to, $email_subject, $email_body, $headers);
+
+    header("Location: index.html");
+
+?>
